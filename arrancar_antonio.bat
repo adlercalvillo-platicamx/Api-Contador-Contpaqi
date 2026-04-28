3c45b1b2bbb58a48e134141a92d9bad71c4c82e5
Adler Antonio Calvillo <adler.antonio@outlook.com>
2026-04-28 13:07:00 -0600

    Bat actualizado con manejo de errores

--- arrancar_antonio.bat
+++ arrancar_antonio.bat
@@ -2,4 +2,10 @@
 cd C:\PlaticaMX\AntonioAPIConsultas
 call venv\Scripts\activate
 start uvicorn main:app --port 8000
+
+:esperar
+timeout /t 2 /nobreak
+curl -s http://localhost:8000/docs >nul 2>&1
+if errorlevel 1 goto esperar
+
 ngrok.exe http --url=pancreas-village-trickery.ngrok-free.dev http://127.0.0.1:8000
